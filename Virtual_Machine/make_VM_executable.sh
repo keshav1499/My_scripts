635eb70b6eb2a810bbaa738c11a41e0da5f7ab22
keshav1499 <keshav1499@gmail.com>
2026-01-17 16:24:13 +0530

    Added CachyOS VM xml packages

---
+++ Virtual_Machine/make_VM_executable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+exec sudo chmod +x /run
+
+exec sudo chmod +x /run/media                                                 
+
+exec sudo chmod +x /run/media/tallguydesi                                     
+
+exec sudo chmod +x /run/media/tallguydesi/Entertainment                         
+
+exec sudo chmod +x /run/media/tallguydesi/Entertainment/Virtual_Machines        
+
+exec sudo chmod +x /run/media/tallguydesi/Entertainment/Virtual_Machines/Windows
+
+exec sudo chmod +x /run/media/tallguydesi/Entertainment/Virtual_Machines/Windows/win11
